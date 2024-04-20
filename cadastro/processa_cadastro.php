4362fb284ffa92cfdbc444dfb8d92f46dcf3b8b2
Nicolas <nweissbueno@gmail.com>
2024-04-19 22:40:25 -0300

    CPF

--- cadastro/processa_cadastro.php
+++ cadastro/processa_cadastro.php
@@ -1,42 +1,44 @@
 
 <?php
+include '/xampp/htdocs/Projeto/bd/connection.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include '/xampp/htdocs/Projeto/bd/connection.php';
-    if ($conn->connect_error) {
-        die("Conexão falhou: " . $conn->connect_error);
-    }
-    $nome = $_POST['Nome'];
-    $email = $_POST['email'];
-    $senha = $_POST['Senha'];
-    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-    $cpf = $_POST['cpf'];
-    $dt_nasc = $_POST['dt_nasc'];
-    $sexo = $_POST['sexo'];
-    $CEP = $_POST['cep'];
-    $problema_saude = $_POST['problema_saude'];
-    $alergias = $_POST['alergias'];
-    $peso = $_POST['peso'];
-    $altura = $_POST['altura'];
+if (!$conn) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo"conectado ao banco";
+$cpf = $_POST['cpf'];
+$cpf = mysqli_real_escape_string($conn, $cpf);
+
+$sql = "SELECT cpf FROM cliente WHERE cpf= '$cpf'";
+$retorno = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($retorno) > 0) {
+    echo"CPF Já cadastrado";
+} else {
+    
+$nome = $_POST['Nome'];
+$email = $_POST['email'];
+$senha = $_POST['Senha'];
+$senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+$cpf = $_POST['cpf'];
+$dt_nasc = $_POST['dt_nasc'];
+$sexo = $_POST['sexo'];
+$CEP = $_POST['cep'];
+$problema_saude = $_POST['problema_saude'];
+$alergias = $_POST['alergias'];
+$peso = $_POST['peso'];
+$altura = $_POST['altura'];
 
 // telefone	dt_nasc	altura	sexo	senha	freq_atv_fisica	email	cep	obj	cpf	
 
-    $sql = "INSERT INTO grafico (peso) VALUES ('$peso')";
+$sql = "INSERT INTO grafico (peso) VALUES ('$peso')";
 
-    $sql = "INSERT INTO cliente (Nome, email, senha, cpf, dt_nasc, sexo, CEP, problema_saude, alergias, altura) 
-    VALUES ('$nome', '$email', '$senhaHash', '$cpf', '$dt_nasc', '$sexo', '$CEP',  '$problema_saude', 
-    '$alergias', '$altura')";
-
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Cadastro realizado com sucesso";
-        header("Location: confirmarcadastro.php");
-    } else {
-        echo "Erro ao cadastrar: " . $conn->error;
-    }
-
-    $conn->close();
-} else {
-    echo "O formulário não foi submetido";
+$sql = "INSERT INTO cliente (Nome, email, senha, cpf, dt_nasc, sexo, CEP, problema_saude, alergias, altura) 
+VALUES ('$nome', '$email', '$senhaHash', '$cpf', '$dt_nasc', '$sexo', '$CEP',  '$problema_saude', 
+'$alergias', '$altura')";
 }
+$resultado = mysqli_query($conn, $sql);
+echo"Usuario cadastrado!!<BR>";
+echo"<a href='confirmarcadastro.php'>AVANÇAR</a>;
+$conn->close();
 ?>
