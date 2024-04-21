618edac9ca03d18e95f9de0dac10302d99768e79
embozza <153663385+embozza@users.noreply.github.com>
2024-04-21 02:32:18 -0300

    logincliente.php

--- login/logincliente.php
+++ login/logincliente.php
@@ -5,18 +5,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 
-    $result = mysqli_query($conn, "SELECT * FROM cliente WHERE email='$email' AND senha='$senha'");
+    $result = mysqli_query($conn, "SELECT * FROM cliente WHERE email='$email'");
 
-    if (mysqli_num_rows($result) > 0) {
-        session_start();
-        $_SESSION['email'] = $email;
-        $_SESSION['nome'] = $row['nome'];
-        echo'login bem sucedido';
-        header('Location: /Projeto/index.php');
-        exit();
+    if ($result !== false && mysqli_num_rows($result) > 0) {
+        $usuario = mysqli_fetch_assoc($result);
+
+        // Verifica a senha usando password_verify
+        if (password_verify($senha, $usuario['senha'])) {
+            // Senha correta, faça o login
+            session_start();
+            $_SESSION['email'] = $email;
+            $_SESSION['nome'] = $usuario['nome']; // Se 'nome' é o campo correto para o nome do cliente
+            echo 'Login bem sucedido';
+            header('Location: /Projeto/index.php');
+            exit();
+        } else {
+            // Senha incorreta
+            echo "<div class='message'>
+            <p>Senha incorreta.</p>
+            </div> <BR>";
+            echo "<a href='entrarcliente.php'>Voltar</a>";
+        }
     } else {
+        // Usuário não encontrado
         echo "<div class='message'>
-        <p>Email ou senha incorretas</p>
+        <p>Usuário não encontrado.</p>
         </div> <BR>";
         echo "<a href='entrarcliente.php'>Voltar</a>";
     }
